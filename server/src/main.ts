97647477dd55812f3e80591e94c1fe3f98cadaba
Keyhan Mohammadi <105974026+mrcimiyagar@users.noreply.github.com>
2023-10-24 15:11:46 +0330

    enabled cors.

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import Drivers from './drivers';
 async function bootstrap() {
   await Drivers.initalize();
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   await app.listen(10000);
 }
 bootstrap();
